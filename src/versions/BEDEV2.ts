0fb5622a905782b4103f38cebb2869522036d2d0
Julli4n <11952329+Julli4n@users.noreply.github.com>
2023-11-15 06:29:41 -0700

    implement new errors from look-api and call-api

--- src/versions/BEDEV2.ts
+++ src/versions/BEDEV2.ts
@@ -1,48 +1,70 @@
 import { type AnyError } from "../types.ts";
 import { parseAnyError } from "../utils/parseAnyError.ts";
 
-type BEDEV2ErrorResponse = string | string[] | {
-  errors?: {
+type BEDEV2ErrorResponse =
+  | string
+  | string[]
+  | {
+    errors?:
+      | {
+        code?: number | string;
+        message?: string;
+      }[]
+      | Record<string, string[]>;
+  }
+  | {
+    error?: string;
+    error_description?: string;
+  }
+  | {
     code?: number | string;
     message?: string;
-  }[] | Record<string, string[]>;
-} | {
-  error?: string;
-  error_description?: string;
-} | {
-  code?: number | string;
-  message?: string;
-  errors?: {
-    code?: number | string;
+    errors?: {
+      code?: number | string;
+      message?: string;
+      field?: string;
+    }[];
+  }
+  | {
+    error?: string;
+    code?: number;
     message?: string;
+  }
+  | {
+    errorCode?: string;
+    errorMessage?: string;
+    failureReason?: number;
     field?: string;
-  }[];
-} | {
-  error?: string;
-  code?: number;
-  message?: string;
-} | {
-  errorCode?: string;
-  errorMessage?: string;
-  failureReason?: number;
-  field?: string;
-  hint?: string | null;
-} | {
-  errorCode?: number;
-  message?: string;
-} | {
-  Error?: {
-    Code?: number;
-    Message?: string;
+    hint?: string | null;
+    clientHint?: string | null;
+  }
+  | {
+    errorCode?: number;
+    message?: string;
+  }
+  | {
+    Error?: {
+      Code?: number;
+      Message?: string;
+    };
+  }
+  | {
+    error?: string;
+    message?: string;
+    errorDetails?: { errorDetailType: string; datastoreErrorCode: string }[];
+  }
+  | {
+    status?: string;
+    message?: string;
+  }
+  | {
+    errorType: string;
+    errorMessage?: string;
+  }
+  | {
+    status_code?: number;
+    message?: string;
   };
-} | {
-  error?: string;
-  message?: string;
-  errorDetails?: { errorDetailType: string; datastoreErrorCode: string }[];
-} | {
-  status?: string;
-  message?: string;
-};
 
 export function parseBEDEV2ErrorFromJSON(
   json: BEDEV2ErrorResponse,
@@ -50,9 +72,11 @@ export function parseBEDEV2ErrorFromJSON(
   try {
     if (typeof json === "string") {
       // in some contextes, it can be a string
-      return [{
-        message: json,
-      }];
+      return [
+        {
+          message: json,
+        },
+      ];
     } else if (Array.isArray(json)) {
       return json.map((errorMessage) => ({
         message: errorMessage,
@@ -75,95 +99,123 @@ export function parseBEDEV2ErrorFromJSON(
           }
           return errors;
         } else {
-          return Object.entries(
-            (json.errors) as Record<
-              string,
-              string[]
-            >,
-          ).map(([index, value]) => ({
-            message: value.join(","),
-            field: index,
-          }));
+          return Object.entries(json.errors as Record<string, string[]>).map(
+            ([index, value]) => ({
+              message: value.join(","),
+              field: index,
+            }),
+          );
         }
       }
       if ("error" in json) {
         if ("code" in json) {
-          return [{
-            code: json.code,
-            message: (json.error as string),
-          }];
+          return [
+            {
+              code: json.code,
+              message: json.error as string,
+            },
+          ];
         } else if ("error_description" in json) {
-          return [{
-            code: json.error,
-            message: json.error_description!,
-          }];
+          return [
+            {
+              code: json.error,
+              message: json.error_description!,
+            },
+          ];
         } else if ("message" in json) {
-          return [{
-            code: json.error,
-            message: json.message!,
-            childErrors: "errorDetails" in json
-              ? json.errorDetails!.map((error) => {
-                return {
-                  type: error.errorDetailType,
-                  code: error.datastoreErrorCode,
-                };
-              })
-              : [],
-          }];
+          return [
+            {
+              code: json.error,
+              message: json.message!,
+              childErrors: "errorDetails" in json
+                ? json.errorDetails!.map((error) => {
+                  return {
+                    type: error.errorDetailType,
+                    code: error.datastoreErrorCode,
+                  };
+                })
+                : [],
+            },
+          ];
         }
       }
 
       if ("message" in json) {
         if ("errorCode" in json) {
-          return [{
-            code: json.errorCode,
-            message: json.message!,
-          }];
+          return [
+            {
+              code: json.errorCode,
+              message: json.message!,
+            },
+          ];
         } else if ("code" in json) {
-          return [{
-            code: json.code,
-            message: json.message!,
-          }];
+          return [
+            {
+              code: json.code,
+              message: json.message!,
+            },
+          ];
         } else if ("status" in json) {
+          return [
+            {
+              code: json.status!,
+              message: json.message!,
+            },
+          ];
+        } else if ("status_code" in json) {
           return [{
-            code: json.status!,
-            message: json.message!,
+            code: json.status_code,
+            message: json.message ?? "???",
           }];
         }
       }
 
       if ("errorMessage" in json) {
-        return [{
-          code: json.errorCode ?? json.failureReason,
-          message: json.errorMessage!,
-          field: json.field,
-          hint: json.hint ?? undefined,
-        }];
+        if ("errorType" in json) {
+          return [
+            {
+              code: json.errorType,
+              message: json.errorMessage ?? "???",
+            },
+          ];
+        } else {
+          return [
+            {
+              code: json.errorCode ?? json.failureReason,
+              message: json.errorMessage!,
+              field: json.field,
+              hint: json.hint ?? json.clientHint,
+            },
+          ];
+        }
       }
 
       if ("Error" in json) {
-        if (
-          ("Code" in json.Error!) &&
-          ("Message" in json.Error!)
-        ) {
-          return [{
-            code: json.Error.Code,
-            message: json.Error.Message!,
-          }];
+        if ("Code" in json.Error! && "Message" in json.Error!) {
+          return [
+            {
+              code: json.Error.Code,
+              message: json.Error.Message!,
+            },
+          ];
         }
       }
 
       if ("code" in json) {
         // nothing else..
-        return [{
-          code: json.code!,
-          message: json.code?.toString() ?? "???",
-        }];
+        return [
+          {
+            code: json.code!,
+            message: json.code?.toString() ?? "???",
+          },
+        ];
       }
 
-      return [{
-        message: "Unknown Error",
-      }];
+      return [
+        {
+          message: "Unknown Error",
+        },
+      ];
     }
   } catch {
     return [];
@@ -181,7 +233,8 @@ export function parseBEDEV2ErrorFromString(text: string, contentType: string) {
 
 export function parseBEDEV2Error(response: Response) {
   return parseAnyError(
-    () => response.clone().text().then((text) => text.trim()),
+    () =>
+      response.clone().text().then((text) => text.trim()),
     parseBEDEV2ErrorFromJSON,
     response.headers,
   );
@@ -191,9 +244,5 @@ export function parseBEDEV2ErrorFromStringAndHeaders(
   text: string,
   headers: Headers,
 ) {
-  return parseAnyError(
-    () => text.trim(),
-    parseBEDEV2ErrorFromJSON,
-    headers,
-  );
+  return parseAnyError(() => text.trim(), parseBEDEV2ErrorFromJSON, headers);
 }
