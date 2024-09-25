208e83004097bde00957ee76224d200d903196db
Julli4n <11952329+Julli4n@users.noreply.github.com>
2024-09-25 23:41:26 +0000

    Build: (a6db709) add new challenge types to type

--- esm/src/utils/parseChallengeHeaders.d.ts
+++ esm/src/utils/parseChallengeHeaders.d.ts
@@ -1,7 +1,7 @@
 export declare const GENERIC_CHALLENGE_ID_HEADER: "rblx-challenge-id";
 export declare const GENERIC_CHALLENGE_TYPE_HEADER: "rblx-challenge-type";
 export declare const GENERIC_CHALLENGE_METADATA_HEADER: "rblx-challenge-metadata";
-type ChallengeType = "captcha" | "forceauthenticator" | "forcetwostepverification" | "securityquestions" | "reauthentication" | "proofofwork" | "rostile" | "privateaccesstoken" | "deviceintegrity";
+type ChallengeType = "captcha" | "forceauthenticator" | "forcetwostepverification" | "securityquestions" | "reauthentication" | "proofofwork" | "rostile" | "privateaccesstoken" | "deviceintegrity" | "emailverification" | "phoneverification";
 export type ParsedChallenge = {
     challengeType: ChallengeType;
     challengeId: string;
